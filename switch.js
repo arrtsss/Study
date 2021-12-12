0050b6eb0cc9f3363df0768cb10340238c63e45d
arrtsss <95982777+arrtsss@users.noreply.github.com>
2021-12-12 21:41:00 +0200

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,18 @@
+let number = +prompt("Введите число");
+switch (number) {
+case 0:
+alert('Вы Ввели 0');
+break;
+
+case 1:
+alert('Вы Ввели 1');
+break;
+
+case 2:
+case 3:
+alert('Вы Ввели 2,а может и 3');
+
+break;
+
+
+}
